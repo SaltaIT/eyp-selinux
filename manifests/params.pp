ea82279d08a295b173e9740b6c11ed9901763e3e
Jordi Prats <jprats@systemadmin.es>
2015-12-16 14:27:28 +0100

    inicial

---
+++ manifests/params.pp
@@ -0,0 +1,37 @@
+class selinux::params {
+
+  case $::osfamily
+  {
+    'redhat':
+    {
+      $selinux_utils='libselinux-utils'
+      case $::operatingsystemrelease
+      {
+        /^[6-7].*$/:
+        {
+        }
+        default: { fail("Unsupported RHEL/CentOS version! - $::operatingsystemrelease")  }
+      }
+    }
+    'Debian':
+    {
+      case $::operatingsystem
+      {
+      'Ubuntu':
+      {
+        case $::operatingsystemrelease
+        {
+          /^14.*$/:
+          {
+            $selinux_utils='selinux-utils'
+          }
+          default: { fail("Unsupported Ubuntu version! - $::operatingsystemrelease")  }
+        }
+      }
+      'Debian': { fail("Unsupported")  }
+      default: { fail("Unsupported Debian flavour!")  }
+      }
+    }
+    default: { fail("Unsupported OS!")  }
+  }
+}
