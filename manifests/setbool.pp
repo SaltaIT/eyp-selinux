2fca2c64711bbb8fba1bc51e85801cdc43697645
Jordi Prats <jprats@systemadmin.es>
2017-06-15 13:08:48 +0200

    dependencia paquet

--- manifests/setbool.pp
+++ manifests/setbool.pp
@@ -11,9 +11,20 @@ define selinux::setbool (
                           $value    = true,
                         ) {
   #
+  include ::selinux
+
+  # policycoreutils
+  if(!defined(Package['policycoreutils']))
+  {
+    package { 'policycoreutils':
+      ensure => 'installed',
+    }
+  }
+
   exec { "selinux::setbool ${boolname} ${value}":
     command => inline_template('setsebool <%= @boolname %> <%= scope.function_bool2onoff([@value]) %>'),
-    unless => inline_template('getsebool <%= @boolname %> | grep <%= scope.function_bool2onoff([@value]) %>'),
-    path => '/bin:/sbin:/usr/bin:/usr/sbin',
+    unless  => inline_template('getsebool <%= @boolname %> | grep <%= scope.function_bool2onoff([@value]) %>'),
+    path    => '/bin:/sbin:/usr/bin:/usr/sbin',
+    require => Package['policycoreutils'],
   }
 }
